d9d880858cb2448ea43e50dd2bd4d74e74125a66
HW <el2bkn2benjan@gmail.com>
2025-06-29 01:01:06 -0400

    Creación de método dividirse junto a su visualización en grilla y visualización de bacterias muertas en main

--- bacteria.py
+++ bacteria.py
@@ -14,15 +14,29 @@ class Bacteria:
         self.energia += consumo
         return consumo
 
+# Método de reproducción de la bacteria
     def dividirse(self):
-        pass
+        if self.energia >= 70:
+            energia_division = self.energia // 2 # Divide su energía a la mitad
+            self.energia = energia_division
+
+            nueva_bacteria = Bacteria(
+                id=f"{self.id}-hija",
+                raza=self.raza,
+                energia=energia_division,
+                resistente=self.resistente,
+                estado="activa"
+            )
+            return nueva_bacteria
+        else:
+            print(f"{self.id} no tiene suficiente energía para dividirse.")
+            return None
 
     def morir(self):
         if self.energia < 10: # Si la energía es menor a 10, la bacteria muere
             self.estado = "muerta"
             return True
         return False
-
 
     def mutar(self):
         pass
--- main.py
+++ main.py
@@ -6,33 +6,41 @@ def grilla_puntos(grilla): # grilla más clara con puntos
     for fila in grilla:
         for objeto in fila:
             if isinstance(objeto, Bacteria):
-                print("BR" if objeto.resistente else "B", end=" ")
+                if objeto.estado == "muerta":
+                    print("X", end=" ") # Bacterias muertas con X
+                else:
+                    print("BR" if objeto.resistente else "B", end=" ")
             else:
                 print(".", end=" ") # Espacios vacios con punto
         print()
 
-# Bacteria no resistente
+# Bacteria a dividir
 bacteria1 = Bacteria( 
     id="bact-1",
     raza="E. coli",
-    energia=50,
+    energia=80, # Energía suficiente para dividirse
     resistente=False,
     estado="activa"
 )
 
-# Bacteria resistente
 bacteria2 = Bacteria(
     id="bact-2",
     raza="Salmonella",
-    energia=30,
+    energia=5, # Energía insuficiente para sobrevivir
     resistente=True,
     estado="activa"
 )
 
 ambiente = Ambiente(filas=5, columnas=5)
 
-ambiente.grilla[0][0] = bacteria1
-ambiente.grilla[1][1] = bacteria2
+ambiente.grilla[0, 0] = bacteria1
+ambiente.grilla[1, 2] = bacteria2
+
+bacteria2.morir() # Bacteria2 muere por falta de energía
+nueva_bacteria = bacteria1.dividirse() # Crear una nueva bacteria
+ambiente.grilla[1, 1] = nueva_bacteria
 
 print("Estado inicial de la grilla:")
 grilla_puntos(ambiente.grilla)
+
+print(f"\nenergia de bacteria1: {bacteria1.energia}, energia de nueva_bacteria: {nueva_bacteria.energia}")
